70359ac683059bccad60a05d2119496a0e3e1164
ani213 <vermaaniket213@gmail.com>
2023-06-15 22:41:55 +0530

    swagger added

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const connection=require("./config/connection");
-const modelInit=require("./models/init")()
-const routes=require('./routes');
+const connection = require("./config/connection");
+const modelInit = require("./models/init")()
+const routes = require('./routes');
 const cors = require('cors');
-const auth=require("./auth")();
-const validation=require("./middleware/validateMiddelware");
-const fileUpload=require('express-fileupload');
+const auth = require("./auth")();
+const validation = require("./middleware/validateMiddelware");
+const fileUpload = require('express-fileupload');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const PORT = process.env.PORT || 8080
+
+const options = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: 'ProblemHub',
+            version: '1.0.0',
+            description: "About : - Documentation of application FlexFit which is a Fitness class Booking application in which you can hire Top quality Trainers or become a Trainer itself.",
+        },
+        servers: [{ url: `http://localhost:${PORT}`, }],
+        securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                description: 'JWT authorization of an API',
+                name: 'Authorization',
+                in: 'header',
+            },
+        },
+    },
+    apis: ["./swagger/*.js"]
+}
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use(fileUpload({
-    useTempFiles:true
+    useTempFiles: true
 }))
 app.use(cors())
 app.use(bodyParser.json());
@@ -18,12 +44,11 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use("/uploads",express.static('uploads'));
-let router=express.Router()
-routes(router,auth,validation);
-app.get("/",(req,res)=>{
-    res.send({message:"App is working fine :-)"})
+app.use("/uploads", express.static('uploads'));
+let router = express.Router()
+routes(router, auth, validation);
+app.get("/", (req, res) => {
+    res.send({ message: "App is working fine :-)" })
 })
-app.use('/app',router)
-const PORT=process.env.PORT||8080
-app.listen(PORT,console.log(`server start:#${PORT}`));
+app.use('/app', router)
+app.listen(PORT, console.log(`server start:#${PORT}`));
