5415cb7feb56ab0af02742368d1f9564bbbd3042
ani213 <vermaaniket213@gmail.com>
2023-07-17 00:56:52 +0530

    comment added

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-require('dotenv').config()  
-module.exports= mongoose.connect(process.env.CONNECTION,
-{ useNewUrlParser: true,useUnifiedTopology: true},(err)=>{
-    if(!err){
-        console.log("successfully connected")
-    }
-    else{
-        console.log("error in database connection",err)
-    }
-})
+require('dotenv').config();
+// CONNECTION=mongodb://localhost:27017/social  
+module.exports = mongoose.connect(process.env.CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+        if (!err) {
+            console.log("successfully connected")
+        }
+        else {
+            console.log("error in database connection", err)
+        }
+    })
